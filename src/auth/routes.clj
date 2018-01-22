025722da1860fc53124c05fb2e717212aa922bd9
Leonardo Iacovini <leoiacovini@gmail.com>
2018-01-22 01:29:04 -0200

    Allow requests and responses in differents content-types

--- src/auth/routes.clj
+++ src/auth/routes.clj
@@ -2,6 +2,7 @@
   (:require [common-labsoft.pedestal.interceptors.auth :as int-auth]
             [common-labsoft.pedestal.interceptors.error :as int-err]
             [common-labsoft.pedestal.interceptors.schema :as int-schema]
+            [common-labsoft.pedestal.interceptors.adapt :as int-adapt]
             [auth.adapters.token :as adapters.token]
             [auth.controllers.user :as controllers.user]
             [auth.controllers.token :as controllers.token]
@@ -36,7 +37,8 @@
 (defroutes routes
   [[["/" ^:interceptors [int-err/catch!
                          (body-params/body-params)
-                         http/json-body
+                         int-adapt/coerce-body
+                         int-adapt/content-neg-intc
                          int-auth/auth
                          int-schema/coerce-output]
      ["/api"
