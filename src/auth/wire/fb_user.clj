58a487fabba52022080a1bf60592148dcc5cce4c
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-01-26 17:01:11 -0200

    Update fb_user skeleton

--- src/auth/wire/fb_user.clj
+++ src/auth/wire/fb_user.clj
@@ -2,6 +2,8 @@
   (:require [schema.core :as s]
             [common-labsoft.schema :as schema]))
 
-(def fb-user-skeleton {:fb-user/id    {:schema s/Str :required true}
-                       :fb-user/email {:schema s/Str :required true}})
+(def fb-user-skeleton {:fb-user/id          {:schema s/Str :required true}
+                       :fb-user/email       {:schema s/Str :required true}
+                       :fb-user/name        {:schema s/Str :required true}
+                       :fb-user/picture-url {:scehma s/Str :required true}})
 (s/defschema FbUser (schema/skel->schema fb-user-skeleton))
