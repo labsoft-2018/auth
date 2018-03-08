49a047eb3fd51cf4721fe875d686e0fc60e161d6
Leonardo Iacovini <leoiacovini@gmail.com>
2018-03-08 03:29:30 -0300

    Bump common-labsoft and update services password, making 10x faster than before

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                                     :password :env/clojars_password}]]
   :plugins [[lein-midje "3.2.1"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [labsoft-2018/common-labsoft "0.9.0-SNAPSHOT"]]
+                 [labsoft-2018/common-labsoft "0.10.0-SNAPSHOT"]]
   :resource-paths ["resources" "config"]
   :min-lein-version "2.0.0"
   :profiles {:dev     {:aliases      {"run-dev" ["trampoline" "run" "-m" "auth.service/start!"]}
